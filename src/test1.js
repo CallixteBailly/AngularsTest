fb95befee525bfef42f0aad8491726d1ea545bb4
Anthony Bailly <Anthony-bailly@live.fr>
2016-01-13 15:54:41 +0100

    Update test1.js

--- src/test1.js
+++ src/test1.js
@@ -22,3 +22,33 @@ angular.module('MyApp',[])
 	
 	
 });
+angular.module('MyApp2', ['ngAnimate', 'ui.bootstrap'])
+.controller('AlertControlle', function ($scope) {
+  $scope.alerts = [
+    { type: 'success', msg: 'Hello AngularsJs.' }
+
+  ];
+
+  
+  $scope.i = 0
+  $scope.addAlert = function() {
+    $scope.i ++
+    $scope.alerts.push({type: 'danger',msg: 'Alert('+ $scope.i +')'});
+  };
+
+  $scope.singleModel  = 1;
+  $scope.hide = function(){
+      if (!$scope.singleModel) {
+        $scope.alerts.push({type: '', msg: 'Success !'});
+      }else
+      {
+        $scope.alerts.splice($scope.i + 1, 1);
+        $scope.isHide = false 
+      }
+  }
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+    $scope.i --;
+  };
+});
